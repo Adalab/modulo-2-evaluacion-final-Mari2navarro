0ffff3df69a37fab80601d02afe2fd3439bf1eb8
Mari <navarro.ortiz.maricarmen@gmail.com>
2026-03-31 18:06:45 +0200

    all my favorites reunited

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,25 @@
 "use strict";
-//SECCIÓN DE QUERY-SELECTOR
-//elementos que nos traemos de HTML
+
+//SECCIÓN DE QUERY-SELECTOR//
 const form = document.querySelector(".js__form");
 const input = document.querySelector(".js__input");
 const resultsList = document.querySelector(".js__results");
+const favoritesList = document.querySelector(".js__favorites");
 
-//SECCIÓN DE DATOS
+//SECCIÓN DE DATOS//
 //Variables globales que almacenan la información principal y se usan por todo el fichero
 let series = [];
+let favorites = [];
 
-//SECCIÓN DE FUNCIONES
+//SECCIÓN DE FUNCIONES//
 //Estas son funciones con código auxiliar, con código que usaremos en los eventos o para pintar en la página
 
 function renderSeries() {
   resultsList.innerHTML = "";
 
-  for (const item of series) {
+  for (let index = 0; index < series.length; index++) {
+    const item = series[index];
+
     const title = item.show.name;
 
     let image = "";
@@ -26,15 +30,38 @@ function renderSeries() {
       image = "https://placehold.co/210x295/f5f5f5/666666/?text=TV";
     }
 
+    const found = favorites.find((fav) => fav.show.id === item.show.id);
+
+    let className = "";
+
+    if (found) {
+      className = "selected";
+    }
+
     resultsList.innerHTML += `
-    <li>
+    <li class="js__series ${className}" data-index="${index}">
     <img src="${image}" />
     <h3>${title}</h3>
     </li>`;
   }
 }
 
+//función para pintar los favoritos
+function renderFavorites() {
+  favoritesList.innerHTML = "";
+
+  for (let i = 0; i < favorites.length; i++) {
+    const fav = favorites[i];
+
+    favoritesList.innerHTML += `
+    <li>${fav.show.name}</li>
+    `;
+  }
+}
+
 //SECCIÓN DE FUNCIONES DE EVENTOS
+
+//Función para traer el api
 function handleSearch(ev) {
   ev.preventDefault();
 
@@ -48,12 +75,47 @@ function handleSearch(ev) {
     });
 }
 
+//Función para acceder al index de series y guardar en favoritos
+function handleClickSeries(ev) {
+  const clickedElement = ev.target.closest(".js__series");
+
+  if (!clickedElement) {
+    return;
+  }
+
+  const index = parseInt(clickedElement.dataset.index);
+  const selectedSerie = series[index];
+
+  const found = favorites.find((fav) => fav.show.id === selectedSerie.show.id);
+
+  if (!found) {
+    favorites.push(selectedSerie);
+  }
+
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+  //llamamos a la función de pintar favoritos
+  renderFavorites();
+  renderSeries();
+}
+
 //SECCIÓN DE EVENTOS
 //Estos son los eventos a los que reacciona la página
+
+//Este evento es para cuando le damos a buscar
 form.addEventListener("submit", handleSearch);
-//escucha que cuando se pulsa enviar sale un mensaje en la consola
+
+//Este evento es para cuando hacemos click en una serie
+resultsList.addEventListener("click", handleClickSeries);
 
 //SECCIÓN DE ACCIONES AL CARGAR LA PÁGINA
 //Este código se ejecutará cuando se carga la página: pedir datos al servidor, pintar elementos en la página
+
+const savedFavorites = JSON.parse(localStorage.getItem("favorites"));
+
+if (savedFavorites) {
+  favorites = savedFavorites;
+  renderFavorites();
+  renderSeries();
+}
 
 console.log("JS conectado");
